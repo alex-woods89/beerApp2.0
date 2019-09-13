c9906bb3853800324b9918b02b31b7a3cb66a123
Alex Woods <alex.woods89@hotmail.co.uk>
2019-09-13 17:04:42 +0100

    added beer list and list item

--- src/components/BeerList.vue
+++ src/components/BeerList.vue
@@ -1,12 +1,19 @@
 <template>
-    <select v-on:change="handleChange" v-model="selectedBeer">
-        <option v-for="(beer, index) in beers" :value="beer">{{beer.name}}</option>
-    </select>    
+    <div>
+        <ul>
+            <list-component v-for="(beer, index) in beers" :beer="beer" :key:"index"></list-component>
+        </ul>
+    </div> 
 </template>
 
 <script>
+  import ListComponent from './ListItem'
 export default {
-
+  name: 'beer-list',
+  props: ['beers'],
+  components: {
+      "list-component": ListComponent
+  }
 }
 </script>
 
---
+++ src/components/ListItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <li v-on:click="handleClick">{{beer.name}}</li>
+</template>
+
+<script>
+import {eventBus} from '../main'
+
+export default {
+  name: 'list-component',
+  props:['beer'],
+  methods: {
+      handleClick() {
+          eventBus.$emit('beer-selected', this.beer)
+      }
+  }
+}
+</script>
+
+<style>
+
+</style>
