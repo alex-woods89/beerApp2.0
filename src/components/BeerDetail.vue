140ac8af867175cf5dcf83a6315202f9a5659489
Alex Woods <alex.woods89@hotmail.co.uk>
2019-09-13 16:54:57 +0100

    beer detail addedd

--- src/components/BeerDetail.vue
+++ src/components/BeerDetail.vue
@@ -1,10 +1,19 @@
 <template>
-  
+      <article v-if="beer">  
+     <h3>{{beer.name}}</h3>
+     <div>
+         <p>ABV: {{beer.abv}}%</p>
+         <img :src="beer.image_url"  height="300">
+         <p>{{beer.description}}</p>        
+     </div>
+    </article>
 </template>
 
 <script>
-export default {
 
+export default {
+  name: 'beer-detail',
+  props:['beer']
 }
 </script>
 
---
+++ src/components/BeerList.vue
@@ -0,0 +1,15 @@
+<template>
+    <select v-on:change="handleChange" v-model="selectedBeer">
+        <option v-for="(beer, index) in beers" :value="beer">{{beer.name}}</option>
+    </select>    
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
